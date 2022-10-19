a9ecce3139024f34dfa7e5842c7680e7a7a4ea5a
FabioPin <fajos.pinheiro@hotmail.com>
2022-10-19 15:56:28 +0100

    Added Sibling Component

--- src/components/Parent.js
+++ src/components/Parent.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import { Child } from './Child';
+import React from "react";
+import { Child } from "./Child";
+import { Sibling } from "./Sibling";
 
 export class Parent extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { 
-        name: 'Frarthur',
-        
+    this.state = {
+      name: "Frarthur",
     };
 
     //function to pass to child
-    this.changeName = this.changeName.bind(this)
+    this.changeName = this.changeName.bind(this);
   }
 
   changeName(newName) {
     this.setState({
-        name: newName
+      name: newName,
     });
   }
 
   render() {
-    return <Child name={this.state.name} onChange={this.changeName} />
+    return (
+      <div>
+        <Child name={this.state.name} onChange={this.changeName} />
+        <Sibling />
+      </div>
+    );
   }
 }
